7b29298c6b1dbaa4778515a0c75a3aff68ef7262
Carlos Henriquez <carlos.henriquez@gmail.com>
2020-12-02 18:13:23 -0300

    hola mundo

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
-
-print ("Server lanzado")
-httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+print ("hola mundo")
