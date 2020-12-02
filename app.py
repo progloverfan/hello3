a344893a3ae0c452f642e01e95a089e12c740fc7
Carlos Henriquez <carlos.henriquez@gmail.com>
2020-12-02 17:41:04 -0300

    app cambiado

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("app.py ejecutado")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+print ("Server lanzado")
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
