dbf8772d965d2b9c29cfca24eb92cb9d3e109999
Carlos Henriquez <carlos.henriquez@gmail.com>
2020-12-03 10:03:33 -0300

    flask y odo gitignore

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print ("hola mundo")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
